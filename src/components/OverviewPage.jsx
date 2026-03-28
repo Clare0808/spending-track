b5f37c42555215f8898412b109383962ad2d938d
Clare0808 <yuhsin099@gmail.com>
2026-03-29 01:28:47 +0800

    create overview page and payment page layout

--- src/components/OverviewPage.jsx
+++ src/components/OverviewPage.jsx
@@ -1,9 +1,82 @@
+import style from "./OverviewPage.module.css"
+
+import pieChart	from "../assets/piechart.png"
+import lineChart from "../assets/linechart.png"
+
+import { SiMealie } from "react-icons/si";
+import { FaCarAlt } from "react-icons/fa";
+
 function OverViewPage () {
-    return (
-        <>
-            <div>OverView</div>
-        </>
-    )
+	return (
+		<>
+			<div className={style.page}>
+				<div className={style.pageTitle}>Clare, 歡迎回來!</div>
+				<div className={style.eleFrameInfo}>
+					<div className={style.boxOutframe}>
+						<div className={style.infoBox}>
+							<div className={style.infoBoxTitle}>總額</div>
+							<div className={style.infoBoxContent}>NT$4,000</div>
+							<div className={style.infoBoxTime}>2026-03-28</div>
+						</div>
+						<div className={style.boxFrame}>
+							<div className={style.infoBox}>
+								<div className={style.infoBoxTitle}>收入</div>
+								<div className={style.infoBoxContent}>NT$4,000</div>
+								<div className={style.infoBoxTime}>2026-03-28</div>
+							</div>
+							<div className={style.infoBox}>
+								<div className={style.infoBoxTitle}>支出</div>
+								<div className={style.infoBoxContent}>NT$4,000</div>
+								<div className={style.infoBoxTime}>2026-03-28</div>
+							</div>
+						</div>
+					</div>
+					<div className={style.chartFrame}>
+						<div className={style.chartTitle}>支出分布</div>
+						<img src={pieChart} className={style.pieChart}/>
+					</div>
+				</div>
+				<div className={style.eleFrameSpending}>
+					<div className={style.spending}>
+						<div className={style.spendingOutframe}>
+							<div className={style.spendingBox}>
+								<SiMealie className={style.icon} />
+								<div className={style.spendingInfo}>
+									<div className={style.spendingTitle}>Dinner</div>
+									<div className={style.spendingDate}>2026-03-28</div>
+								</div>
+							</div>
+							<div className={style.spendingPrice}>NT$200</div>
+						</div>
+						<div className={style.spendingOutframe}>
+							<div className={style.spendingBox}>
+								<FaCarAlt className={style.icon} />
+								<div className={style.spendingInfo}>
+									<div className={style.spendingTitle}>Car Expense</div>
+									<div className={style.spendingDate}>2026-03-28</div>
+								</div>
+							</div>
+							<div className={style.spendingPrice}>NT$200</div>
+						</div>
+						<div className={style.spendingOutframe}>
+							<div className={style.spendingBox}>
+								<FaCarAlt className={style.icon} />
+								<div className={style.spendingInfo}>
+									<div className={style.spendingTitle}>Car Expense</div>
+									<div className={style.spendingDate}>2026-03-28</div>
+								</div>
+							</div>
+							<div className={style.spendingPrice}>NT$200</div>
+						</div>
+					</div>
+					<div className={style.chartFrame}>
+						<div className={style.chartTitle}>支出分布</div>
+						<img src={lineChart} className={style.lineChart}/>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default OverViewPage
--- src/components/PaymentPage.jsx
+++ src/components/PaymentPage.jsx
@@ -1,9 +1,118 @@
+import style from "./PaymentPage.module.css";
+
+import { SiMealie } from "react-icons/si";
+import { FaCarAlt } from "react-icons/fa";
+import { IoSchool } from "react-icons/io5";
+import { FaCalendar } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
+
 function PaymentPage () {
-    return (
-        <>
-            <div>Transaction</div>
-        </>
-    )
+	return (
+		<>
+			<div className={style.page}>
+				<div className={style.spending}>
+					<div className={style.spendingOutframe}>
+						<div className={style.spendingBox}>
+							<SiMealie className={style.icon} />
+							<div className={style.spendingInfo}>
+								<div className={style.spendingTitle}>Dinner</div>
+								<div className={style.spendingTime}>2026-03-28</div>
+							</div>
+						</div>
+						<div className={style.spendingPrice}>NT$200</div>
+					</div>
+					<div className={style.spendingOutframe}>
+						<div className={style.spendingBox}>
+							<FaCarAlt className={style.icon} />
+							<div className={style.spendingInfo}>
+								<div className={style.spendingTitle}>Car Expense</div>
+								<div className={style.spendingTime}>2026-03-28</div>
+							</div>
+						</div>
+						<div className={style.spendingPrice}>NT$200</div>
+					</div>
+					<div className={style.spendingOutframe}>
+						<div className={style.spendingBox}>
+							<SiMealie className={style.icon} />
+							<div className={style.spendingInfo}>
+								<div className={style.spendingTitle}>Dinner</div>
+								<div className={style.spendingTime}>2026-03-28</div>
+							</div>
+						</div>
+						<div className={style.spendingPrice}>NT$200</div>
+					</div>
+				</div>
+				<div className={style.dataInputFrame}>
+					<div className={style.createSpendingOutframe}>
+						<div className={style.spendingBox}>
+							<SiMealie className={style.icon} />
+							<div className={style.spendingInfo}>
+								<div className={style.spendingTitle}>Dinner</div>
+								<div className={style.spendingTime}>2026-03-28</div>
+							</div>
+						</div>
+						<div className={style.spendingPrice}>NT$200</div>
+					</div>
+					<div className={style.typeBox}>
+						<div className={style.type}>收入</div>
+						<div className={style.type}>支出</div>
+					</div>
+					<div className={style.inputFrame}>
+						<FaCalendar className={style.calendarIcon} />
+						<div className={style.inputInFrame}>
+							<div className={style.inputBox}>
+								<input type="text" placeholder="請輸入內容..." className={style.input} />
+								<input type="text" placeholder="請輸入金額..." className={style.input} />
+							</div>
+							<div className={style.btnFrame}>
+								<div className={style.calculaterFrame}>
+									<div className={style.calculaterLine}>
+										<div className={style.calculaterEle}>1</div>
+										<div className={style.calculaterEle}>2</div>
+										<div className={style.calculaterEle}>3</div>
+										<div className={style.calculaterOperator}>+</div>
+									</div>
+									<div className={style.calculaterLine}>
+										<div className={style.calculaterEle}>4</div>
+										<div className={style.calculaterEle}>5</div>
+										<div className={style.calculaterEle}>6</div>
+										<div className={style.calculaterOperator}>-</div>
+									</div>
+									<div className={style.calculaterLine}>
+										<div className={style.calculaterEle}>7</div>
+										<div className={style.calculaterEle}>8</div>
+										<div className={style.calculaterEle}>9</div>
+										<div className={style.calculaterOperator}>*</div>
+									</div>
+									<div className={style.calculaterLine}>
+										<div className={style.calculaterEle}>.</div>
+										<div className={style.calculaterZero}>0</div>
+										<div className={style.calculaterOperator}>/</div>
+									</div>
+								</div>
+								<div className={style.optionsBox}>
+									<div className={style.option}>
+										<SiMealie className={style.optionIcon} />
+										餐飲
+									</div>
+									<div className={style.option}>
+										<FaCarAlt className={style.optionIcon} />
+										交通
+									</div>
+									<div className={style.option}>
+										<IoSchool className={style.optionIcon} />
+										教育
+									</div>
+								</div>
+							</div>
+						</div>
+						<FaCheck className={style.checkIcon} />
+					</div>
+					
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default PaymentPage
